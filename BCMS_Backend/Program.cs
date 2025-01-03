5692862de9eb8d293c5d9d30cb0488b0d11ffd98
uzername <ivanpost777@gmail.com>
2025-01-04 00:45:55 +0200

    Added bypass for CORS. Added Frontend project to get values from Web Api

--- BCMS_Backend/Program.cs
+++ BCMS_Backend/Program.cs
@@ -7,12 +7,27 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+// Add CORS policy to allow all origins. CORS is annoying
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", builder =>
+    {
+        builder.AllowAnyOrigin() // Allow requests from any origin
+               .AllowAnyHeader() // Allow any headers
+               .AllowAnyMethod(); // Allow any HTTP methods (GET, POST, etc.)
+    });
+});
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 // I remove that some day
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+// Enable CORS and shut down restrictions
+app.UseCors("AllowAll");
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
