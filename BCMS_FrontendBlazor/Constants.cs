46fc67c7d0a21b5c07dacf78260b48759717735b
uzername <ivanpost777@gmail.com>
2025-01-08 23:27:30 +0200

    I try send request to api

---
+++ BCMS_FrontendBlazor/Constants.cs
@@ -0,0 +1,10 @@
+﻿namespace BCMS_FrontendBlazor
+{
+    public class APIConstantsDef  {
+        public static String API_ROOT = "api";
+
+        public static String API_BOOK = "book";
+        public static String API_BOOK_forUI = "forUI";
+        public static String API_BOOK_upload = "upload";
+    }
+}
