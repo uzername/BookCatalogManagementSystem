71eb3b65ac4b969f61dfc753cec22ca55ae24f98
uzername <ivanpost777@gmail.com>
2025-01-15 23:24:20 +0200

    added New Book page . Added limit rate code. It should limit abuse of API but I am not sure. Anyway instructions were clear

--- BCMS_Backend/Program.cs
+++ BCMS_Backend/Program.cs
@@ -1,8 +1,26 @@
 // this file is weird because I forgot to enable some option on project init heheh
 using BCMS_Backend.Repository;
 using BCMS_Backend.Settings;
+using Microsoft.AspNetCore.RateLimiting;
+using System.Threading.RateLimiting;
 
 var builder = WebApplication.CreateBuilder(args);
+// rate limiting
+// https://learn.microsoft.com/en-us/aspnet/core/performance/rate-limit?view=aspnetcore-9.0
+// https://medium.com/@nirajranasinghe/secure-your-asp-net-core-app-with-rate-limiting-middleware-for-efficient-resource-allocation-5032e9d07531
+/*
+ PermitLimit to 4 and the time Window to 12. A maximum of 4 requests per each 12-second window are allowed.
+QueueProcessingOrder to OldestFirst.
+QueueLimit to 2.
+ */
+builder.Services.AddRateLimiter(_ => _
+    .AddFixedWindowLimiter(policyName: "fixed", options =>
+    {
+        options.PermitLimit = 4;
+        options.Window = TimeSpan.FromSeconds(12);
+        options.QueueProcessingOrder = QueueProcessingOrder.OldestFirst;
+        options.QueueLimit = 2;
+    }));
 
 // Add services to the container.
 
@@ -25,6 +43,8 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+// also for rate limiting
+app.UseRateLimiter();
 
 // Enable CORS and shut down restrictions
 app.UseCors("AllowAll");
