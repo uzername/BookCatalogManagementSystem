dc650246cbc812a895c9c09693b0247d7b9ba33c
uzername <ivanpost777@gmail.com>
2025-01-18 13:40:08 +0200

    Added something about Docker

---
+++ BCMS_FrontendBlazor/script.sh
@@ -0,0 +1,3 @@
+﻿#!/bin/sh
+# vim:sw=2:ts=2:sts=2:et
+jq --arg aVar "$(printenv API_BASE_URL)" '.ApiBaseUrl = $aVar' /usr/share/nginx/html/appsettings.json > /usr/share/nginx/html/appsettings.json.tmp && mv /usr/share/nginx/html/appsettings.json.tmp /usr/share/nginx/html/appsettings.json
